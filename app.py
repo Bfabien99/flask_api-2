66f1344b68ef70ecc05738c0d81843c04b849f01
bfabien99 <fabienbrou99@gmail.com>
2022-12-09 11:54:53 +0000

    import sqlite3 lib and create function to connect to db

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
 from flask import Flask, request, jsonify
+import json
+import sqlite3
 
 app = Flask(__name__)
 
-books_list = [
-    {"id": 1, "author": "Chinua Achebe",
-        "language": "English", "title": "Things Fall Apart"},
-    {"id": 2, "author": "Saint Augustine", "language": "English",
-        "title": "Saint Augustine’s Confessions"},
-    {"id": 3, "author": "Cicero", "language": "English", "title": "On Obligations"},
-    {"id": 4, "author": "Livy", "language": "English", "title": "The Rise of Rome"},
-    {"id": 5, "author": "Lucretius", "language": "English",
-        "title": "On the Nature of the Universe"},
-    {"id": 6, "author": "Marcus Aurelius",
-        "language": "English", "title": "Meditations"},
-    {"id": 7, "author": "Ovid", "language": "English", "title": "Metamorphoses"},
-    {"id": 8, "author": "Tacitus", "language": "English",
-        "title": "Agricola and Germany"},
-    {"id": 9, "author": "Virgil", "language": "English", "title": "Georgics"},
-    {"id": 10, "author": "Pierrot", "language": "French",
-        "title": "Au clair de la lune"}
-]
+def db_connect():
+    conn = None
+    try:
+        conn = sqlite3.connect('books.sqlite')
+    except sqlite3.error as e:
+        print(e)
+    return conn
 
 
 @app.route('/books', methods=['GET', 'POST'])
